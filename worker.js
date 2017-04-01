83d3ad93cf8a1793c338d661a2bfcc59707a6746
Fabian Schneider <fabbbbbi+git@googlemail.com>
2017-04-01 09:40:32 +0200

    Fix Linting Issues

--- worker.js
+++ worker.js
@@ -9,8 +9,8 @@ NopeTweet
   .find()
   .then(items => {
     tweets = items
-    const originalCheck = items.map(item => item._doc.id_str && fetch(`https://twitter.com/anyuser/status/${item._doc.id_str}`) || {})
-    const nopeCheck = items.map(item => item._doc.nope && fetch(`https://twitter.com/anyuser/status/${item._doc.nope.id_str}`) || {})
+    const originalCheck = items.map(item => (item._doc.id_str && fetch(`https://twitter.com/anyuser/status/${item._doc.id_str}`)) || {})
+    const nopeCheck = items.map(item => (item._doc.nope && fetch(`https://twitter.com/anyuser/status/${item._doc.nope.id_str}`)) || {})
     return Promise.all([Promise.all(originalCheck), Promise.all(nopeCheck)])
   })
   .then(items => {
